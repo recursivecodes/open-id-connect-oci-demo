f40ed1b8a14404b2cd41440e9f108e62b392cd27
recursivecodes <toddraymondsharp@gmail.com>
2021-04-19 21:00:56 -0400

    update app.yml, home view, add tracing, add endpoints

--- src/main/java/codes/recursive/controller/PageController.java
+++ src/main/java/codes/recursive/controller/PageController.java
@@ -1,5 +1,6 @@
 package codes.recursive.controller;
 
+import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.annotation.*;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
@@ -16,5 +17,17 @@ public class PageController {
     @Get(uri="/")
     public Map<String, Object> index() {
         return new HashMap<>();
+    }
+
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Get("/secure")
+    public Map<String, Object> secured() {
+        return CollectionUtils.mapOf("secured", true);
+    }
+
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    @Get("/error")
+    public Map<String, Object> error() {
+        return CollectionUtils.mapOf("error", true);
     }
 }
